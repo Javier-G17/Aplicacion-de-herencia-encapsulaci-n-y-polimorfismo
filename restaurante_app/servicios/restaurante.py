ea09ffb18a61b18e5aa4bce9162b725c376f46ec
Javier-G17 <javiergarciabg632.com>
2026-07-11 17:01:52 -0500

    Polimorfismo agregado

--- restaurante_app/servicios/restaurante.py
+++ restaurante_app/servicios/restaurante.py
@@ -14,6 +14,14 @@ class Restaurante:
         self.productos.append(producto)
         print("\nProducto registrado correctamente.")
 
+    def registrar_platillo(self, platillo):
+        self.productos.append(platillo)
+        print("\nPlatillo registrado correctamente.")
+
+    def registrar_bebida(self, bebida):
+        self.productos.append(bebida)
+        print("\nBebida registrada correctamente.")
+
     def mostrar_productos(self):
         """
         Muestra todos los productos registrados.
@@ -26,4 +34,20 @@ class Restaurante:
             return
 
         for producto in self.productos:
-            producto.mostrar_informacion()
+                producto.mostrar_informacion()
+
+    def mostrar_platillo(self):
+        print("\n==========PLATILLOS==========")
+        if not self.platillos:
+            print("No existen platillos registrados")
+            for platillo in self.platillos:
+                platillo.mostrar_informacion() 
+            
+    def mostrar_bebida(self):
+        print("\n==========BEBIDAS==========")
+        if not self.bebidas:
+            print("No existen bebidas registrados")
+            for bebida in self.bebidas:
+                bebida.mostrar_informacion() 
+
+    
